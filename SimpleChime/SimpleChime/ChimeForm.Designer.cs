4bf4260f870dbe66bae7a75ff6cb1d28481b90fc
Andrew Willms <AndrewW@hyivy.com>
2024-01-03 00:25:27 -0500

    bunch of initial work

--- SimpleChime/SimpleChime/ChimeForm.Designer.cs
+++ SimpleChime/SimpleChime/ChimeForm.Designer.cs
@@ -1,6 +1,12 @@
-﻿namespace SimpleChime;
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace SimpleChime;
+
+
 
 partial class ChimeForm {
+
 	/// <summary>
 	///  Required designer variable.
 	/// </summary>
@@ -24,11 +30,92 @@ partial class ChimeForm {
 	///  the contents of this method with the code editor.
 	/// </summary>
 	private void InitializeComponent() {
-		this.components = new System.ComponentModel.Container();
-		this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-		this.ClientSize = new System.Drawing.Size(800, 450);
-		this.Text = "Form1";
+		TimerPeriodInput = new NumericUpDown();
+		TimerPeriodLabel = new Label();
+		TimeToRingLabel = new Label();
+		ResetTimerButton = new Button();
+		RemoveTimeButton = new Button();
+		AddTimeButton = new Button();
+		((System.ComponentModel.ISupportInitialize)TimerPeriodInput).BeginInit();
+		SuspendLayout();
+		// 
+		// TimerPeriodInput
+		// 
+		TimerPeriodInput.Location = new Point(94, 63);
+		TimerPeriodInput.Name = "TimerPeriodInput";
+		TimerPeriodInput.Size = new Size(120, 23);
+		TimerPeriodInput.TabIndex = 0;
+		TimerPeriodInput.ValueChanged += TimerPeriodInput_ValueChanged;
+		// 
+		// TimerPeriodLabel
+		// 
+		TimerPeriodLabel.AutoSize = true;
+		TimerPeriodLabel.Location = new Point(220, 65);
+		TimerPeriodLabel.Name = "TimerPeriodLabel";
+		TimerPeriodLabel.Size = new Size(38, 15);
+		TimerPeriodLabel.TabIndex = 1;
+		TimerPeriodLabel.Text = "label1";
+		// 
+		// TimeToRingLabel
+		// 
+		TimeToRingLabel.AutoSize = true;
+		TimeToRingLabel.Location = new Point(94, 106);
+		TimeToRingLabel.Name = "TimeToRingLabel";
+		TimeToRingLabel.Size = new Size(38, 15);
+		TimeToRingLabel.TabIndex = 2;
+		TimeToRingLabel.Text = "label2";
+		// 
+		// ResetTimerButton
+		// 
+		ResetTimerButton.Location = new Point(220, 102);
+		ResetTimerButton.Name = "ResetTimerButton";
+		ResetTimerButton.Size = new Size(75, 23);
+		ResetTimerButton.TabIndex = 3;
+		ResetTimerButton.Text = "Reset Timer";
+		ResetTimerButton.UseVisualStyleBackColor = true;
+		// 
+		// RemoveTimeButton
+		// 
+		RemoveTimeButton.Location = new Point(94, 147);
+		RemoveTimeButton.Name = "RemoveTimeButton";
+		RemoveTimeButton.Size = new Size(75, 23);
+		RemoveTimeButton.TabIndex = 4;
+		RemoveTimeButton.Text = "- 10 mins";
+		RemoveTimeButton.UseVisualStyleBackColor = true;
+		// 
+		// AddTimeButton
+		// 
+		AddTimeButton.Location = new Point(220, 147);
+		AddTimeButton.Name = "AddTimeButton";
+		AddTimeButton.Size = new Size(75, 23);
+		AddTimeButton.TabIndex = 5;
+		AddTimeButton.Text = "+ 10 mins";
+		AddTimeButton.UseVisualStyleBackColor = true;
+		// 
+		// ChimeForm
+		// 
+		AutoScaleDimensions = new SizeF(7F, 15F);
+		AutoScaleMode = AutoScaleMode.Font;
+		ClientSize = new Size(800, 450);
+		Controls.Add(AddTimeButton);
+		Controls.Add(RemoveTimeButton);
+		Controls.Add(ResetTimerButton);
+		Controls.Add(TimeToRingLabel);
+		Controls.Add(TimerPeriodLabel);
+		Controls.Add(TimerPeriodInput);
+		Name = "ChimeForm";
+		Text = "SimpleChime";
+		((System.ComponentModel.ISupportInitialize)TimerPeriodInput).EndInit();
+		ResumeLayout(false);
+		PerformLayout();
 	}
 
 	#endregion
+
+	private NumericUpDown TimerPeriodInput;
+	private Label TimerPeriodLabel;
+	private Label TimeToRingLabel;
+	private Button ResetTimerButton;
+	private Button RemoveTimeButton;
+	private Button AddTimeButton;
 }
--- SimpleChime/SimpleChime/ChimeForm.cs
+++ SimpleChime/SimpleChime/ChimeForm.cs
@@ -1,11 +1,87 @@
+using System;
+using System.Windows.Forms;
+
 namespace SimpleChime;
 
 
 
 public partial class ChimeForm : Form {
-	
+
+	private decimal TimerPeriod = 45;
+
 	public ChimeForm() {
 		InitializeComponent();
+
+		TimerPeriodInput.Value = 45;
+
+		Timer MyTimer = new();
+		MyTimer.Interval = (45 * 60 * 1000); // 45 mins
+		MyTimer.Tick += new(TimerRing);
+		MyTimer.Start();
+	}
+
+	private void TimerPeriodInput_ValueChanged(object sender, EventArgs e) {
+
+		decimal newTimerPeriod = TimerPeriodInput.Value;
+
+		decimal currentTimerValue = 
+
+	}
+
+	private void ResetTime() {
+
+		Timer timer = new();
+		timer.Interval = (45 * 60 * 1000); // 45 mins
+		timer.Tick += new(TimerRing);
+		timer.Start();
+
+		timer.
+
+	}
+
+	private void TimerRing(object sender, EventArgs e) {
+
+	}
+
+
+
+}
+
+public class TimerRapper {
+
+	public bool IsRunning { get; private set; } = false;
+
+	public TimeSpan? Duration { get; private set; }
+
+	public decimal TimeElapsed { get; private set; } = 0;
+
+	public decimal TimeLeft { get; private set; } = 0;
+
+
+
+	private Timer InternalTimer = new();
+
+	private DateTime? TimeWhenStarted = null;
+
+	private Action Callback;
+
+
+
+	public TimerRapper() {
+
+	}
+
+	public void SetTimer(TimeSpan duration, Action onRing) {
+
+		Duration = duration;
+		TimeWhenStarted = DateTime.Now;
+
+	}
+
+	public void TimerRings() {
+
+
+
 	}
 
 }
